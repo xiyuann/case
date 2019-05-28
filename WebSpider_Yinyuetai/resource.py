619f9911895036f1a9d0baff10225e6e0623cd33
Sheehan Yuan <45458151+sheehanyuan@users.noreply.github.com>
2019-05-28 22:27:30 +0800

    Create resource.py

---
+++ WebSpider_Yinyuetai/resource.py
@@ -0,0 +1,66 @@
+Resource.py
+# _*_coding:utf-8_*_
+# Editor  : SHEEHAN YUAN
+# Date    : 2019/5/20 18:30
+# File    : resource.py
+# IDE     : PyCharm
+UserAgents = [
+    'Mozilla/5.0 (Linux; Android 4.1.1; Nexus 7 Build/JRO03D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Safari/535.19',
+    'Mozilla/5.0 (Linux; U; Android 4.0.4; en-gb; GT-I9300 Build/IMM76D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
+    'Mozilla/5.0 (Linux; U; Android 2.2; en-gb; GT-P1000 Build/FROYO) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1',
+    'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:21.0) Gecko/20100101 Firefox/21.0',
+    'Mozilla/5.0 (Android; Mobile; rv:14.0) Gecko/14.0 Firefox/14.0',
+    'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.94 Safari/537.36',
+    'Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19',
+    'Mozilla/5.0 (iPad; CPU OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3',
+    'Mozilla/5.0 (iPod; U; CPU like Mac OS X; en) AppleWebKit/420.1 (KHTML, like Gecko) Version/3.0 Mobile/3A101a Safari/419.3',
+    'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0;',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv,2.0.1) Gecko/20100101 Firefox/4.0.1',
+    'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
+    'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)',
+]
+PROXIES = [  '111.155.116.210:8123'
+    '61.135.217.7:80'
+    '122.114.31.177:808'
+    '27.15.22.242:8118'
+    '121.31.195.245:8123'
+    '218.20.218.59:8118'
+    '58.216.202.149:8118'
+    '180.113.168.23:8118'
+    '58.249.99.188:8118'
+    '111.192.179.38:8118'
+    '111.155.116.200:8123'
+    '221.195.11.152:80'
+    '180.118.242.95:61234'
+    '122.7.178.49:8118'
+    '60.179.40.157:33404'
+    '27.159.167.96:49330'
+    '60.23.38.52:80'
+    '111.155.116.208:8123'
+    '180.172.159.5:8118'
+    '111.155.116.217:8123'
+    '121.31.102.230:8123'
+    '171.39.45.223:8123'
+    '221.224.62.182:3128'
+    '222.137.200.128:8118'
+    '110.73.5.206:8123'
+    '60.168.87.15:808'
+    '182.35.144.113:8118'
+    '125.109.197.48:23643'
+    '182.34.48.112:41480'
+    '106.58.123.223:80'
+    '113.121.241.218:808'
+    '183.165.77.18:8118'
+    '180.158.109.60:8118'
+    '114.231.153.193:20013'
+    '115.217.253.61:808'
+    '58.48.88.140:8118'
+    '180.118.241.139:61234'
+    '171.39.28.93:8123'
+    '180.119.65.169:808'
+    '111.155.116.220:8123'
+    '110.189.207.77:29977'
+    '42.225.138.157:8118'
+    '115.151.205.150:808'
+    '111.155.116.211:8123']
